da64dc6fb29aa0c11dfdb72b941a093d1fb0f178
xhaios89438 <jackisusing@gmail.com>
2026-06-30 14:40:05 +0100

    Update lightbox.js

--- js/lightbox.js
+++ js/lightbox.js
@@ -4,40 +4,118 @@ const overlay = document.getElementById("lightbox-overlay");
 
 const image = document.getElementById("lightbox-image");
 
+const secondImage = document.getElementById("second-image");
+
 const title = document.getElementById("lightbox-title");
+
+const description = document.getElementById("project-description");
 
 const link = document.getElementById("project-link");
 
 const closeBtn = document.getElementById("close-btn");
 
+
+
+
+
 items.forEach(item => {
+
 
     item.addEventListener("click", () => {
 
+
+
         image.src = item.dataset.image;
+
+
+        secondImage.src = item.dataset.second;
+
+
 
         title.textContent = item.dataset.title;
 
+
+
+        description.textContent = item.dataset.description;
+
+
+
         link.href = item.dataset.link;
 
-        overlay.style.display = "flex";
+
+
+        overlay.classList.add("active");
+
+
+
+        document.body.style.overflow = "hidden";
+
 
     });
 
-});
-
-closeBtn.addEventListener("click", () => {
-
-    overlay.style.display = "none";
 
 });
+
+
+
+
+
+
+
+function closeLightbox(){
+
+
+    overlay.classList.remove("active");
+
+
+    document.body.style.overflow = "";
+
+
+}
+
+
+
+
+
+
+
+closeBtn.addEventListener("click", closeLightbox);
+
+
+
+
+
+
 
 overlay.addEventListener("click", (e) => {
 
+
     if(e.target === overlay){
 
-        overlay.style.display = "none";
+
+        closeLightbox();
+
 
     }
+
+
+});
+
+
+
+
+
+
+
+document.addEventListener("keydown", (e)=>{
+
+
+    if(e.key === "Escape"){
+
+
+        closeLightbox();
+
+    }
+
 
 });
